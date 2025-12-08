5c2a1f347aa35394b1896a03e6f004271f93453d
ITO <danielviillegas6@gmail.com>
2025-12-07 19:55:01 -0600

    se libero espacio

--- add_pdf_path_to_mrp_oc.sql
+++
@@ -1,2 +0,0 @@
--- Add pdf_path column to mrp_oc table
-ALTER TABLE `mrp_oc` ADD COLUMN `pdf_path` VARCHAR(255) NULL AFTER `eta`;
--- verify_maquiladora_table.sql
+++
@@ -1,7 +0,0 @@
--- Script para verificar la estructura de la tabla maquiladora
--- Ejecuta esto primero para ver el tipo de dato exacto
-
-DESCRIBE maquiladora;
-
--- O alternativamente:
-SHOW CREATE TABLE maquiladora;
